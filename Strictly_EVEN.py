d2d2b58800d769745b6d1c0c76982051dc40baad
Deepika-Gutti <84383398+Deepika-Gutti@users.noreply.github.com>
2023-01-29 13:16:21 +0530

    def strickodd(a):     for i in range(len(a)):         if(a[i]%2==0):             if(i%2!=0):                 return False     return True n=int(input()) a=list(map(int,input().split())) print(strickodd(a))

---
+++ Strictly_EVEN.py
@@ -0,0 +1,10 @@
+def strickodd(a):
+    for i in range(len(a)):
+        if(a[i]%2==0):
+            if(i%2!=0):
+                return False
+    return True
+n=int(input())
+a=list(map(int,input().split()))
+print(strickodd(a))
+    
